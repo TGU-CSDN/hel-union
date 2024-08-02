a06dd7ad295ef366bedd61e92a4cd8bc97117fe6
Cal0rie <zmxianyu@163.com>
2024-08-03 04:57:45 +0800

    feat:调整github pages

--- appInfo.js
+++ appInfo.js
@@ -2,9 +2,9 @@ const helDevUtils = require('hel-dev-utils');
 const pkg = require('./package.json');
 
 // deploy to unpkg
-const appInfo = helDevUtils.createVueSubApp(pkg, { npmCdnType: 'unpkg' });
+// const appInfo = helDevUtils.createVueSubApp(pkg, { npmCdnType: 'unpkg' });
 
 // deploy to git
-// const appInfo = helDevUtils.createVueSubApp(pkg, { defaultHomePage: 'https://hel-eco.github.io/hel-tpl-remote-vue-comp/as_v1' });
+const appInfo = helDevUtils.createVueSubApp(pkg, { defaultHomePage: 'https://tgu-csdn.github.io/hel-union/as_v1' });
 
 module.exports = appInfo;
