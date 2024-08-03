57e82991d22e31e4eedf87db4a2f112932bc3e85
Cal0rie <zmxianyu@163.com>
2024-08-03 17:01:55 +0800

    feat:update

--- appInfo.js
+++ appInfo.js
@@ -5,6 +5,6 @@ const pkg = require('./package.json');
 // const appInfo = helDevUtils.createVueSubApp(pkg, { npmCdnType: 'unpkg' });
 
 // deploy to git
-const appInfo = helDevUtils.createVueSubApp(pkg, { defaultHomePage: 'https://tgu-csdn.github.io/hel-union/as_v1' });
+const appInfo = helDevUtils.createVueSubApp(pkg, { homePage: 'https://tgu-csdn.github.io/hel-union/as_v1' });
 
 module.exports = appInfo;
